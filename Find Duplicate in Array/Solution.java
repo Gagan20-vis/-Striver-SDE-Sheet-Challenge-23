bd686e1c0cdc1d04a9317f020cb7fa0dd31fc541
Gagan <gaganvishwakarma46@gmail.com>
2023-06-10 22:35:52 +0530

    Find Duplicate in Array

---
+++ Find Duplicate in Array/Solution.java
@@ -0,0 +1,19 @@
+public class Solution{
+    public static int findDuplicate(ArrayList<Integer> arr, int n){
+        // Write your code here.
+        int slow = arr.get(0);
+        int fast = arr.get(0);
+        do{
+            slow = arr.get(slow);
+            fast = arr.get(arr.get(fast));
+        }while(slow!=fast);
+
+        fast = arr.get(0);
+        while(slow!=fast)
+        {
+            slow = arr.get(slow);
+            fast = arr.get(fast);
+        }
+        return slow;
+    }
+}
