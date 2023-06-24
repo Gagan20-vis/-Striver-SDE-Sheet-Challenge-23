933dca1570a721feb40d93e18d21f8a59c24f9a2
Gagan <gaganvishwakarma46@gmail.com>
2023-06-24 18:25:57 +0530

    Add Two Numbers

---
+++ Add Two Numbers/Solution.java
@@ -0,0 +1,51 @@
+Node *addTwoNumbers(Node *num1, Node *num2)
+
+        {
+
+        Node*start= new Node();
+
+        Node*temp= start;
+
+        int carry= 0;
+
+        while(num1!=NULL || num2!=NULL || carry){
+
+        int sum= 0;
+
+        if(num1!=NULL){
+
+        sum+=num1->data;
+
+        num1= num1->next;
+
+        }
+
+
+
+        if (num2 != NULL) {
+
+        sum += num2->data;
+
+        num2 = num2->next;
+
+        }
+
+
+
+        sum += carry;
+
+        carry = sum / 10;
+
+
+
+        Node *node = new Node(sum % 10);
+
+        temp->next = node;
+
+        temp = temp->next;
+
+        }
+
+        return start->next;
+
+        }
